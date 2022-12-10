cb188d56637a609b245f0f4af3e1d8705f331137
Ishan Kakadiya <ishankakadiya7@gmail.com>
2022-12-10 13:41:08 +0530

    Life Quotes

---
+++ lib/Globle.dart
@@ -0,0 +1,22 @@
+import 'package:life_quotes_and_sayings/model/quotes.dart';
+
+class Globle {
+  static int index = 0;
+  static bool chaeckFavorite = false;
+  static List favoriteList = [];
+  static List<Quotes> quotesCategory = [];
+  static List imagesRendom = [];
+  static List<Quotes> authoresList = [];
+  static List categoryList = [
+    "Ability",
+    "Action",
+    "Age",
+    "Apology",
+    "Art",
+    "Famous",
+    "Future",
+    "Sucess",
+    "Friendship",
+    "Wisdom",
+  ];
+}
